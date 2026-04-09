772d4cbb88a3bd11b99166f837dde15865b028c9
dreamboy001226 <dreamboy001226@gmail.com>
2026-04-09 09:37:04 +0500

    Update build_prime.sh

--- build_prime.sh
+++ build_prime.sh
@@ -15,7 +15,7 @@ build_kernel() {
 
     BUILD_VAR="-j$(nproc) -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1"
 
-    cat arch/arm64/configs/vendor/kona-not_defonfig arch/arm64/configs/vendor/samsung/$DEVICE.config \
+    cat arch/arm64/configs/vendor/kona-sec-perf_defconfig arch/arm64/configs/vendor/samsung/$DEVICE.config \
         arch/arm64/configs/ksu.config > arch/arm64/configs/temp_defconfig
 
     echo "
